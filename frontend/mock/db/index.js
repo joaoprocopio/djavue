f50bb0769eacb048dcf13c4be88bf6026c499853
joao.procopio <joao.procopio@buser.com.br>
2022-10-30 01:09:20 -0300

    mock: renamed model

--- frontend/mock/db/index.js
+++ frontend/mock/db/index.js
@@ -2,5 +2,5 @@ const fs = require("fs")
 const path = require("path")
 
 module.exports = {
-  users: JSON.parse(fs.readFileSync(path.resolve(__dirname, "users.json"))),
+  users: JSON.parse(fs.readFileSync(path.resolve(__dirname, "user.json"))),
 }
