e4cd0e5e54b20b8ddf58e29c0dfd228761bcb156
joao.procopio <joao.procopio@buser.com.br>
2022-10-30 15:29:57 -0300

    front: added session id cookie to mock

--- frontend/mock/db/index.js
+++ frontend/mock/db/index.js
@@ -2,5 +2,5 @@ const fs = require("fs")
 const path = require("path")
 
 module.exports = {
-  users: JSON.parse(fs.readFileSync(path.resolve(__dirname, "user.json"))),
+  user: JSON.parse(fs.readFileSync(path.resolve(__dirname, "user.json"))),
 }
--- frontend/mock/index.js
+++ frontend/mock/index.js
@@ -1,18 +1,42 @@
+// imports
+const db = require("./db")
 const express = require("express")
+const crypto = require("node:crypto")
 
+// startup and configuration
 const app = express()
 const port = 8001
 
+// middleware
 app.use(express.json())
 
+// routers
 const user = express.Router()
-
 app.use("/api/user", user)
+user.post("/login", (req, res) => {
+  const { username, password } = req.body
 
-user.get("/whoami", (req, res) => {
-  res.status(200).json({ authenticated: false })
+  try {
+    const user = db.user.find(
+      (user) => user.username === username && user.password === password
+    )
+
+    const sessionid = crypto.randomUUID()
+
+    res.cookie("sessionid", sessionid).status(200).send({
+      id: user.id,
+      name: user.name,
+      username: user.username,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+    })
+  } catch (error) {
+    res.status(404).send({ error: error }).end()
+  }
 })
 
+// server
 app.listen(port, () => {
   console.log(`server running on: http://localhost:${port}`)
 })
