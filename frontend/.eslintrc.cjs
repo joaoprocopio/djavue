e3b1edc1426a2abcf74be45b964f16ca87af5be8
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-11-01 23:03:09 -0300

    frontend: added prettier to eslint rules

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -4,8 +4,9 @@ require("@rushstack/eslint-patch/modern-module-resolution")
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
     "eslint:recommended",
+    "plugin:vue/vue3-essential",
+    "plugin:prettier/recommended",
     "@vue/eslint-config-prettier",
   ],
   env: {
