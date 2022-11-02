5471a16fe288b244854d49b975e0545d5f64fc0b
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-11-01 22:08:27 -0300

    frontend: removed text from layouts

--- frontend/src/layouts/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout.vue
@@ -1,9 +1,4 @@
 <template>
-  <header class="mt-8 mb-16 text-center">
-    <span class="font-black text-2xl text-green-700"
-      >I am a Default Layout</span
-    >
-  </header>
   <main>
     <slot />
   </main>
--- frontend/src/layouts/ErrorLayout.vue
+++ frontend/src/layouts/ErrorLayout.vue
@@ -1,7 +1,4 @@
 <template>
-  <header class="mt-8 mb-16 text-center">
-    <span class="font-black text-2xl text-red-700">I am a Error Layout</span>
-  </header>
   <main>
     <slot />
   </main>
