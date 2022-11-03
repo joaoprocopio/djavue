8d2b5f74553aea3a5bd7aa57a83c339445f62363
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-11-02 23:23:55 -0300

    frontend: config for cypress support

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -9,9 +9,12 @@ module.exports = {
     "plugin:prettier/recommended",
     "@vue/eslint-config-prettier",
   ],
-  env: {
-    node: true,
-  },
+  overrides: [
+    {
+      files: ["cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}"],
+      extends: ["plugin:cypress/recommended"],
+    },
+  ],
   parserOptions: {
     ecmaVersion: "latest",
   },
