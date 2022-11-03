ba0c334ec974e480e5360967deb99f8edfcc0182
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-11-02 23:00:37 -0300

    fronted: update on vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,18 +4,20 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "~": fileURLToPath(new URL("./src", import.meta.url)),
-      "@": fileURLToPath(new URL("./", import.meta.url)),
+export default defineConfig(() => {
+  return {
+    server: {
+      host: "0.0.0.0",
     },
-  },
-  build: {
-    sourcemap: true,
-  },
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "~": fileURLToPath(new URL("./src", import.meta.url)),
+        "@": fileURLToPath(new URL("./", import.meta.url)),
+      },
+    },
+    build: {
+      sourcemap: true,
+    },
+  }
 })
