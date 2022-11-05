69db628fcde4ee7c2899de91cbaec2c6218c29fd
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-11-04 21:29:24 -0300

    django: configured cors

--- djavue/settings.py
+++ djavue/settings.py
@@ -41,6 +41,7 @@ THIRD_PARTY_APPS = [
 INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -84,6 +85,10 @@ DATABASES = {
         "PORT": config("DJANGO_DB_PORT", default=5432),
     },
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 
 
 # Password validation
