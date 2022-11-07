a4b85620d00219482f7381eaa4018886167f292f
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-11-07 17:44:48 -0300

    backend: added cookies validation

--- djavue/settings.py
+++ djavue/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+from corsheaders.defaults import default_headers, default_methods
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -88,6 +89,12 @@ DATABASES = {
         "PORT": config("DJANGO_DB_PORT", default=5432),
     },
 }
+
+CORS_ALLOW_HEADERS = list(default_headers)
+
+CORS_ALLOW_METHODS = list(default_methods)
+
+CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
