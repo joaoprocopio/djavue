93ce4eb270a122c3ac6000ab89dd7756029916df
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-11-10 21:02:52 -0300

    test: refactor login tests

--- user/tests/test_user_views.py
+++ user/tests/test_user_views.py
@@ -1,36 +1,47 @@
-import json
-from unittest.mock import ANY
+from json import dumps, loads
 
 from user.views import user_login, user_logout, user_whoami
 
 
 def test_user_login_with_valid_user(rf, user, session_middleware):
-    request = rf.post(
-        "/api/user/login/",
-        data={
+    request_body = dumps(
+        {
             "username": user.username,
             "password": "password",
-        },
+        }
     )
+
+    request = rf.post(
+        "/api/user/login/",
+        request_body,
+        "application/json",
+    )
+
     session_middleware.process_request(request)
     response = user_login(request)
-    response_content = json.loads(response.content)
+    response_content = loads(response.content)
 
     assert response.status_code == 200
     assert user.id == response_content.get("id")
 
 
 def test_user_login_with_invalid_user(rf, user, session_middleware):
+    request_body = dumps(
+        {
+            "username": "",
+            "password": "",
+        }
+    )
+
     request = rf.post(
         "/api/user/login/",
-        data={
-            "username": ANY,
-            "password": ANY,
-        },
+        request_body,
+        "application/json",
     )
+
     session_middleware.process_request(request)
     response = user_login(request)
-    response_content = json.loads(response.content)
+    response_content = loads(response.content)
 
     assert response.status_code == 404
     assert response_content == {}
@@ -41,7 +52,7 @@ def test_user_logout_with_logged_user(rf, user, session_middleware):
     request.user = user
     session_middleware.process_request(request)
     response = user_logout(request)
-    response_content = json.loads(response.content)
+    response_content = loads(response.content)
 
     assert response.status_code == 200
     assert response_content == {}
@@ -52,7 +63,7 @@ def test_user_logout_with_anonymous_user(rf, anonymous_user, session_middleware)
     request.user = anonymous_user
     session_middleware.process_request(request)
     response = user_logout(request)
-    response_content = json.loads(response.content)
+    response_content = loads(response.content)
 
     assert response.status_code == 404
     assert response_content == {}
@@ -62,7 +73,7 @@ def test_user_whoami_with_logged_user(rf, user):
     request = rf.get("/api/user/whoami/")
     request.user = user
     response = user_whoami(request)
-    response_content = json.loads(response.content)
+    response_content = loads(response.content)
 
     assert response.status_code == 200
     assert response_content.get("authenticated") is True
@@ -72,7 +83,7 @@ def test_user_whoami_with_anonymous_user(rf, anonymous_user):
     request = rf.get("/api/user/whoami/")
     request.user = anonymous_user
     response = user_whoami(request)
-    response_content = json.loads(response.content)
+    response_content = loads(response.content)
 
     assert response.status_code == 200
     assert response_content.get("authenticated") is False
