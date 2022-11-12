a3501700b575e2e7b0b05baec554225ca872aff7
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-11-12 11:44:31 -0300

    chore: added storybook

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,6 +1,5 @@
 /* eslint-env node */
 require("@rushstack/eslint-patch/modern-module-resolution")
-
 module.exports = {
   root: true,
   extends: [
@@ -8,6 +7,7 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "plugin:prettier/recommended",
     "@vue/eslint-config-prettier",
+    "plugin:storybook/recommended",
   ],
   overrides: [
     {
