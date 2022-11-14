e73668b6299657a7b7dd1e8b93c8031010824860
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-11-13 23:15:21 -0300

    tests: updated test structure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,6 @@ import vue from "@vitejs/plugin-vue"
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
-    server: {
-      host: "0.0.0.0",
-      port: 3000,
-    },
     plugins: [vue()],
     resolve: {
       alias: {
@@ -19,6 +15,14 @@ export default defineConfig(() => {
     },
     build: {
       sourcemap: true,
+    },
+    server: {
+      host: "0.0.0.0",
+      port: 3000,
+    },
+    test: {
+      root: "src/",
+      environment: "jsdom",
     },
     publicDir: "./src/public",
   }
