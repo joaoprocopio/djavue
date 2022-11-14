187c1d8958453bf04cf3c2ab04e5e612f30b0214
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-11-14 19:48:25 -0300

    style: removed quotes in storybook config

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,17 +1,17 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/vue3",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/vue3",
+  core: {
+    builder: "@storybook/builder-vite"
   },
-  "features": {
-    "storyStoreV7": true
+  features: {
+    storyStoreV7: true
   }
 }
