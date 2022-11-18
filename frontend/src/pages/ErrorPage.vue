0dd9e82668cabe6a9e27a509fe55cd33e5d5b82f
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-11-18 10:57:05 -0300

    feat: working on error page

--- frontend/src/pages/ErrorPage.vue
+++ frontend/src/pages/ErrorPage.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>Page not found</div>
-  <div>
-    Sorry, the page <code>{{ fullPath }}</code> could not be found.
+  <div class="text-3xl font-bold mb-12">Page not found</div>
+  <div class="mb-8">
+    Sorry, the page
+    <code class="px-2 py-1 bg-neutral-100 font-bold rounded">{{
+      fullPath
+    }}</code>
+    could not be found.
   </div>
-  <RouterLink :to="{ name: 'home' }">Go back to the home page</RouterLink>
+  <RouterLink
+    class="underline text-blue-500"
+    :to="{ name: 'home' }"
+    >Go back to the home page</RouterLink
+  >
 </template>
 
 <script setup>
