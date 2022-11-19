6d55784a5a37254ee278f160afb3bc6462a871ae
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-11-19 14:45:51 -0300

    fix: interpreter bug

--- user/service.py
+++ user/service.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 def find_user(username):
     if "@" in username:
         user = User.objects.get(email=username.lower())
-
-    user = User.objects.get(username=username)
+    else:
+        user = User.objects.get(username=username)
 
     return user
