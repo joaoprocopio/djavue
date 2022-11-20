018c980b66dcbb31b0105b04f180428ee57007eb
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-11-20 16:06:40 -0300

    tests: small fix on names and asserts

--- user/tests/test_user_serializer.py
+++ user/tests/test_user_serializer.py
@@ -1,10 +1,19 @@
-from user.serializer import user_to_dict_json
+from user.serializer import find_user_to_dict_json, user_to_dict_json
 
 
-def test_user_to_dict_json(db, user):
+def test_user_to_dict_json(user):
     serialized_user = user_to_dict_json(user)
 
-    assert user.id == serialized_user["id"]
-    assert user.first_name == serialized_user["first_name"]
-    assert user.last_name == serialized_user["last_name"]
-    assert user.email == serialized_user["email"]
+    assert user.id == serialized_user.get("id")
+    assert user.first_name == serialized_user.get("first_name")
+    assert user.last_name == serialized_user.get("last_name")
+    assert user.email == serialized_user.get("email")
+
+
+def test_find_user_to_dict_json(user):
+    serialized_user = find_user_to_dict_json(user)
+
+    assert user.id == serialized_user.get("id")
+    assert user.username == serialized_user.get("username")
+    assert user.first_name == serialized_user.get("first_name")
+    assert user.is_active == serialized_user.get("is_active")
