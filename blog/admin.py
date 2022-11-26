a2044ed9c4114c85503f5fb53b40b35b8e2825d8
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-11-25 21:12:42 -0300

    feat: registered new model on admin

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,12 @@ from blog.models import Post
 
 # Register your models here.
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ("id", "title", "text", "slug")
+    list_display = ("id", "title", "slug")
+    fields = ("id", "author", "title", "slug", "text", "created_at", "posted_at")
+    readonly_fields = ("id", "slug", "created_at", "posted_at")
+
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -9,16 +9,14 @@ from django.utils.crypto import get_random_string
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=64)
-    slug = models.SlugField(max_length=32, null=True, unique=True)
+    slug = models.SlugField(max_length=32, unique=True, null=True, blank=True)
     text = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
-    posted_at = models.DateTimeField(null=True)
-    is_deleted = models.BooleanField(default=False)
+    posted_at = models.DateTimeField(null=True, blank=True)
 
     def post(self):
         self.posted_at = timezone.now()
         self.slug = get_random_string(32)
-        self.save()
 
     def __str__(self):
         return self.title
