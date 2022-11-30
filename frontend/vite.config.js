f1856f01f1b2986c5a70e8fdc898bdca26ec66f7
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-11-30 16:39:33 -0300

    feat: registered vuetify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,13 @@
-import { fileURLToPath, URL } from "node:url"
-
-import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import vuetify from "vite-plugin-vuetify"
+
+import { fileURLToPath, URL } from "node:url"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
-    plugins: [vue()],
+    plugins: [vue(), vuetify({ autoImport: true })],
     resolve: {
       alias: {
         "~": fileURLToPath(new URL("./src", import.meta.url)),
