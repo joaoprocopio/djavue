b9b38e3476f24a7e21b2ff914699fcc8c279775b
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-12-01 22:24:24 -0300

    feat: now posts are ordered

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,3 @@
-from http import HTTPStatus
-
 from django.http import JsonResponse
 
 from blog.models import Post
@@ -9,7 +7,7 @@ from blog.serializer import post_to_dict_json
 
 
 def posts_fetch_all(request):
-    posts = Post.objects.select_related("author").all()
+    posts = Post.objects.select_related("author").all().order_by("posted_at").reverse()
 
     response = [post_to_dict_json(post) for post in posts]
 
