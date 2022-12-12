e45602cc818b853f71a35b2a16e1b3dd85c0fe9d
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-12-12 00:07:22 -0300

    refactor 2

--- blog/views.py
+++ blog/views.py
@@ -23,10 +23,7 @@ def blog_posts(request: WSGIRequest) -> JsonResponse:
 
 @require_GET
 def blog_post(request: WSGIRequest, id: str) -> JsonResponse:
-    if not id:
-        return JsonResponse({}, status=HTTPStatus.NO_CONTENT)
-
-    if not id.isdigit():
+    if not id or id.isdigit():
         return JsonResponse({}, status=HTTPStatus.BAD_REQUEST)
 
     try:
