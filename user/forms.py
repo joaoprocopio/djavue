5494c76012acbcddc57ae7a472bec8a22e8135da
joao.procopio <joao.procopio@buser.com.br>
2022-12-22 13:30:01 -0300

    adiciona validacao

--- user/forms.py
+++ user/forms.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from email_validator import EmailNotValidError, validate_email
 from pydantic import BaseModel, validator
 
 
@@ -8,14 +11,20 @@ class UserRegister(BaseModel):
     first_name: str
     last_name: str
 
-    @validator("username")
-    def username_validator(cls, username):
-        return username
-
     @validator("email")
     def email_validator(cls, email):
-        return email
+        try:
+            validate_email(email)
+
+            return email
+        except EmailNotValidError as error:
+            raise error
 
     @validator("password")
     def password_validator(cls, password):
-        return password
+        try:
+            validate_password(password)
+
+            return password
+        except ValidationError as error:
+            raise error
