13484775a88486bd7beabf4a490f97c3c18be321
joao.procopio <joao.procopio@buser.com.br>
2022-12-22 13:39:57 -0300

    posts/home_page/

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from blog.views import blog_home_page, blog_post
 
 urlpatterns = [
-    path("home_page/", blog_home_page),
+    path("posts/home_page/", blog_home_page),
     path("posts/<int:id>", blog_post),
 ]
