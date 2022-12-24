cb7e24fa79bfb1f757f9594d2c3d5c67b18c6634
joao.procopio <joao.procopio@buser.com.br>
2022-12-24 00:46:09 -0300

    melhora views

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from blog.views import blog_get_post_by_id, blog_get_posts_by_author_id, blog_home_page
 
 urlpatterns = [
-    path("posts/home_page/", blog_home_page),
-    path("posts/user/<int:author_id>", blog_get_posts_by_author_id),
-    path("posts/<int:id>", blog_get_post_by_id),
+    path("home_page/", blog_home_page),
+    path("user/<int:author_id>", blog_get_posts_by_author_id),
+    path("<int:id>", blog_get_post_by_id),
 ]
