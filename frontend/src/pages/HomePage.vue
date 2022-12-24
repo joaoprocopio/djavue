ff2804123074bffbbacdebb257708ef9f446fe1c
joao.procopio <joao.procopio@buser.com.br>
2022-12-24 16:14:24 -0300

    test api

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,4 +1,17 @@
 <template>
-  <section>teste</section>
-  <v-btn>teste</v-btn>
+  <form @submit.prevent="fetch">
+    <input v-model="path" />
+    <button type="submit">teste</button>
+  </form>
 </template>
+
+<script setup>
+import { $axios } from '~/api';
+import { ref } from 'vue';
+
+const path = ref("")
+
+const fetch = () => {
+  $axios.get(path.value).then((r) => console.log(r))
+}
+</script>
