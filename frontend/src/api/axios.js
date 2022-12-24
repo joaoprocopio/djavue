41bc971482cb36b744e9f97cc07528a17081e855
joao.procopio <joao.procopio@buser.com.br>
2022-12-24 14:05:30 -0300

    adiciona base api no axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const $axios = axios.create({
   xsrfHeaderName: "X-CSRFToken",
   xsrfCookieName: "csrftoken",
-  baseURL: "http://localhost:8000",
+  baseURL: "http://localhost:8000/api/",
   withCredentials: true,
   timeout: 60000,
 })
