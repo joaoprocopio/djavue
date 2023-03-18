aeaeb2ddf7ad4c809ca0301dfedbc88dc14e9254
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-03-17 23:56:26 -0300

    reestrutura

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,17 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution")
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:prettier/recommended",
   ],
+  env: {
+    node: true,
+  },
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: "latest",
+  },
 }
---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  semi: false,
+  vueIndentScriptAndStyle: true,
+  bracketSameLine: true,
+}
