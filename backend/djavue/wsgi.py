ed09431b6317f0fae9e19ae0e7bd3a3998599964
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-04-29 19:02:07 -0300

    reformat

--- backend/djavue/wsgi.py
+++ backend/djavue/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djavue.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djavue.settings")
 
 application = get_wsgi_application()
