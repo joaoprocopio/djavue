dd078a10c5df1bc0e12c9c3dfd1a3cad930fff48
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-04-30 00:15:25 -0300

    foi

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,14 +2,18 @@ import { alias, plugins } from "./configs"
 
 import { defineConfig } from "vite"
 
-export default defineConfig({
-  plugins,
-  server: {
-    host: true,
-    port: 3000,
-  },
-  resolve: {
-    alias,
-  },
-  publicDir: "./src/public",
+const config = defineConfig(() => {
+  return {
+    plugins,
+    server: {
+      host: true,
+      port: 3000,
+    },
+    resolve: {
+      alias,
+    },
+    publicDir: "./src/public",
+  }
 })
+
+export default config
