4292877e72ac4101c2c9931199c04849b5401c58
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-05-03 02:35:41 -0300

    n sei pq funcionou

--- frontend/lib/project/env.ts
+++ frontend/lib/project/env.ts
@@ -1,11 +1,5 @@
 export const env = {
-  get DEV(): boolean {
-    return import.meta.env.MODE === "development"
-  },
-  get MOCK(): boolean {
-    return import.meta.env.VITE_MOCK === "true"
-  },
-  get API_URL(): string {
-    return import.meta.env.VITE_API_URL
-  },
+  DEV: import.meta.env.MODE === "development",
+  MOCK: import.meta.env.VITE_MOCK === "true",
+  API_URL: import.meta.env.VITE_API_URL,
 }
