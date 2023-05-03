937d24588292e2f1f6a97dff819b21beb6aabbd2
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-05-03 15:02:01 -0300

    defaults

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -7,6 +7,12 @@ import * as components from "vuetify/components"
 import * as directives from "vuetify/directives"
 
 export const vuetifyPlugin = createVuetify({
+  defaults: {
+    global: {
+      elevation: "0",
+      variant: "text",
+    },
+  },
   components,
   directives,
   icons: {
