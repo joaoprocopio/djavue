04c753394198c5c7e6dcee4a12532611dc0f15b1
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-05-13 21:24:34 -0300

    envs

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,4 +1,11 @@
+/// <reference types="vite/client" />
+
 import "vue-router"
+
+interface ImportMetaEnv {
+  readonly _MOCK: string
+  readonly _API_URL: string
+}
 
 interface RouterLayoutMeta {
   name?: string
--- frontend/vite.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly _MOCK: string
-  readonly _API_URL: string
-}
