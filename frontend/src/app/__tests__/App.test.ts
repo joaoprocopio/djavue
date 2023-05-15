21536521e65cc4a9f8a020af1aed80e4f303e924
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-05-15 01:19:06 -0300

    que bug do caraio

--- frontend/src/app/__tests__/App.test.ts
+++ frontend/src/app/__tests__/App.test.ts
@@ -1,22 +1,18 @@
 import App from "../App.vue"
 
-import { useThemeStore } from "~/stores"
-
 import { render } from "@testing-library/vue"
-import { createTestingPinia } from "@pinia/testing"
 
-const wrapper = render(App, {
-  global: {
-    plugins: [createTestingPinia()],
-  },
-})
-
-const $theme = useThemeStore()
+const component = (options = {}) =>
+  render(App, {
+    ...options,
+  })
 
 describe("App", () => {
   it("Theme renders - light", () => {
-    $theme.current = "light"
+    const { html } = component()
 
-    console.log(wrapper.html())
+    // TODO: Unknown file extension ".css"
+
+    expect(html()).toMatchSnapshot()
   })
 })
