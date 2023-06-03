c590cfb607280e292695a8f3007b685fb1ac3fa4
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-06-02 23:13:08 -0300

    imports bug

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,28 @@
+// TODO: quebrar test para vitest config
+
 import { plugins } from "./lib/project/plugins"
 import { alias } from "./lib/project/alias"
 
 import { defineConfig } from "vite"
 import { configDefaults } from "vitest/config"
 
-export default defineConfig(() => {
-  return {
-    plugins,
-    envPrefix: "_",
-    server: {
-      host: true,
-      port: 3000,
-    },
-    resolve: {
-      alias,
-    },
-    test: {
-      globals: true,
-      environment: "jsdom",
-      exclude: [...configDefaults.exclude, "./e2e/**"],
-    },
-  }
+export default defineConfig({
+  plugins,
+  resolve: {
+    alias,
+  },
+  envPrefix: "_",
+  server: {
+    host: true,
+    port: 3000,
+  },
+  preview: {
+    host: true,
+    port: 3001,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    exclude: [...configDefaults.exclude, "./e2e/**"],
+  },
 })
