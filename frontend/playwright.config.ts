e1a575a89e4924fe9300b9d6820e5eee4a052c95
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-06-02 22:56:55 -0300

    prefer some file extensions

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,47 +1,40 @@
-import type { PlaywrightTestConfig } from "@playwright/test"
-import { devices } from "@playwright/test"
+import { devices, defineConfig } from "@playwright/test"
 
-const config: PlaywrightTestConfig = {
-  testDir: "./tests/e2e",
-  timeout: 30 * 1000,
-  expect: {
-    timeout: 5000,
-  },
+const baseURL = {
+  preview: "http://localhost:3001",
+  dev: "http://localhost:3000",
+}
+
+export default defineConfig({
+  testDir: "./e2e",
+  fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: "html",
   use: {
-    actionTimeout: 0,
-    baseURL: "http://localhost:3000",
+    headless: true,
+    baseURL: process.env.CI ? baseURL.preview : baseURL.dev,
     trace: "on-first-retry",
-    headless: !!process.env.CI,
   },
   projects: [
     {
       name: "chromium",
-      use: {
-        ...devices["Desktop Chrome"],
-      },
+      use: { ...devices["Desktop Chrome"] },
     },
+
     {
       name: "firefox",
-      use: {
-        ...devices["Desktop Firefox"],
-      },
+      use: { ...devices["Desktop Firefox"] },
     },
     {
       name: "webkit",
-      use: {
-        ...devices["Desktop Safari"],
-      },
+      use: { ...devices["Desktop Safari"] },
     },
   ],
   webServer: {
-    command: process.env.CI ? "vite preview --port 3000" : "vite dev",
-    port: 3000,
+    command: process.env.CI ? "npm run preview" : "npm run dev",
+    url: process.env.CI ? baseURL.preview : baseURL.dev,
     reuseExistingServer: !process.env.CI,
   },
-}
-
-export default config
+})
