e51232bf1900959a3c06fb2e2b0a40a56d78aad9
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-07-08 13:36:01 -0300

    pinia

--- frontend/src/plugins/index.ts
+++ frontend/src/plugins/index.ts
@@ -1,7 +1,9 @@
 import { type App } from 'vue'
 
+import { pinia } from './pinia'
 import { vuetify } from './vuetify'
 
 export const install = (app: App) => {
+  app.use(pinia)
   app.use(vuetify)
 }
