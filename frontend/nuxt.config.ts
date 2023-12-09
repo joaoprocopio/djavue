919c6a2690987382f06e69556a83f7e746b3b235
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-12-09 10:28:19 -0300

    html

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            htmlAttrs: {
+                lang: "pt-br"
+            },
+            charset: "utf-8",
+            viewport: "width=device-width, initial-scale=1",
+            title: "Construindo plataformas com qualidade",
+            titleTemplate: "%s | Djavue"
+        }
+    },
     devtools: {
         enabled: true
     },
