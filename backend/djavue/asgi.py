f535b990d0a1ff286c574617fcaff393378d0da4
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-12-09 09:22:12 -0300

    delete backend

--- backend/djavue/asgi.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djavue.settings")
-
-application = get_asgi_application()
--- backend/tasks/services.py
+++
@@ -1,9 +0,0 @@
-from tasks.models import Task
-
-
-def filter_tasks(**kwargs):
-    return Task.objects.filter(**kwargs)
-
-
-def get_task(**kwargs):
-    return Task.objects.get(**kwargs)
